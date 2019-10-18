e71f49d90f17228d09111bd91ab7c989dd0150f4
sunhui <1161186762@qq,com>
2019-10-18 16:33:29 +0800

    11

--- day07/day0706.py
+++ day07/day0706.py
@@ -17,7 +17,7 @@ print(f)
 # 通过生成器可以获取到数据但它不占用额外的空间存储数据
 # 每次需要数据的时候就通过内部的运算得到数据(需要花费额外的时间)
 f = (x ** 2 for x in range(1, 1000))
-print(sys.getsizeof(f))  # 相比生成式生成器不占用存储数据的空间
+print(sys.getsizeof(f))  # 相比生成式生成器不占用存储数据的空间。
 print(f)
 for val in f:
     print(val)
