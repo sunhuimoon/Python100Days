b19e860117b2997f2c5c241f91870539ea7e50c6
sunhui <1161186762@qq,com>
2019-10-22 10:38:49 +0800

    文件与异常

--- day11/day1101.py
+++ day11/day1101.py
@@ -1,7 +1,28 @@
+# 例1
+# def main():
+#     f = open('致橡树.txt', 'r', encoding='utf-8')
+#     print(f.read())
+#     f.close()
+#
+#
+# if __name__ == '__main__':
+#     main()
+
+# 例2
 def main():
-    f = open('致橡树.txt', 'r', encoding='utf-8')
-    print(f.read())
-    f.close()
+    f = None
+    try:
+        f = open('致橡树.txt', 'r', encoding='UTF-8')
+        print(f.read())
+    except FileNotFoundError:
+        print('无法打开指定的文件!')
+    except LookupError:
+        print('指定了未知的编码!')
+    except UnicodeDecodeError:
+        print('读取文件时解码错误!')
+    finally:
+        if f:
+            f.close()
 
 
 if __name__ == '__main__':
